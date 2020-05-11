1e873cff111ff98321c35b1691d79c8b519eae31
bekazandukeli <bzandukeli@vaba.co>
2020-05-11 15:56:57 +0400

    Updates

---
+++ precache-manifest.33f72f903fc227cfed578e5fc493c9a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f279d56dd22e2020e0195c972024564",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "8d59b93b29984457830b",
+    "url": "/covid-19-tracker/static/css/main.d212aa74.chunk.css"
+  },
+  {
+    "revision": "1e882c2b059e0ee97cd9",
+    "url": "/covid-19-tracker/static/js/2.a393519d.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.a393519d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d59b93b29984457830b",
+    "url": "/covid-19-tracker/static/js/main.ca6f896f.chunk.js"
+  },
+  {
+    "revision": "fa0c104a82072024f0ae",
+    "url": "/covid-19-tracker/static/js/runtime-main.5b571555.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19-tracker/static/media/image.d7265326.png"
+  }
+]);
